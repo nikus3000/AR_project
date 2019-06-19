c5ada02bdd206a7c16035a43902f165e372b1f04
Dominika Gronek <nikus3000@gmail.com>
2019-06-19 17:00:03 +0200

    first commit

---
+++ Assets/Scripts/MyScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    protected Joystick joystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.velocity = new Vector3(joystick.Horizontal,
+                                         0.0f,
+                                         joystick.Vertical);
+    }
+}
